9f4c00d2d128bfbb167ea237a7b2c45993f6e79d
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-31 15:39:42 -0800

    create layout

---
+++ Soundboard/RecordViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecordViewController.swift
+//  Soundboard
+//
+//  Created by Mariano Echegoyen on 12/31/17.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
