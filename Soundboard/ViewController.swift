9680b64e7bb689132d513667b7b7084b05c7ad3b
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-31 12:14:12 -0800

    Initial Commit

---
+++ Soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soundboard
+//
+//  Created by Mariano Echegoyen on 12/31/17.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
