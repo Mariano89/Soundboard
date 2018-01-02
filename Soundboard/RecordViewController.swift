696f7969e669701351f04f5bf75133230195e9f4
Mariano Echegoyen <mariano.echegoyen@dng.com>
2018-01-02 00:25:37 -0800

    save audio and title to core data

--- Soundboard/RecordViewController.swift
+++ Soundboard/RecordViewController.swift
@@ -82,6 +82,20 @@ class RecordViewController: UIViewController {
         }
     }
     @IBAction func addTapped(_ sender: Any) {
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            let sound = Sound(entity: Sound.entity(), insertInto: context)
+            sound.name = nameTextField.text
+            
+            if let audioURL = self.audioURL {
+                sound.audioData = try? Data(contentsOf: audioURL)
+                try? context.save()
+                navigationController?.popViewController(animated: true)
+            }
+            
+        }
+        
     }
     
 }
